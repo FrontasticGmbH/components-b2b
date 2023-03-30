9cffd70efaa5c170fa90273a01b0c8130ae76e7a
Carlos Mourullo <carlosmourullo@gmail.com>
2023-03-30 11:53:11 +0200

    feat: updated filed to prepare b2b for mutitenant

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const { i18n, localePath } = require('./next-i18next.config');
 const withPWA = require('next-pwa');
+const { i18n, localePath } = require('./next-i18next.config');
 
 module.exports = withPWA({
+  productionBrowserSourceMaps: true,
   pwa: {
     dest: 'public',
-    disable: true,
-    register: false,
+    disable: process.env.NODE_ENV === 'development',
   },
   i18n,
   localePath,
@@ -16,11 +16,21 @@ module.exports = withPWA({
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // path: `https://res.cloudinary.com/dlwdq84ig/image/upload/`,
   },
-  webpack(config) {
+  webpack(config, { webpack, buildId }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    if (buildId !== 'development') {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env': {
+            NEXT_PUBLIC_EXT_BUILD_ID: JSON.stringify(process.env.NEXT_PUBLIC_EXT_BUILD_ID),
+          },
+        }),
+      );
+    }
 
     return config;
   },
