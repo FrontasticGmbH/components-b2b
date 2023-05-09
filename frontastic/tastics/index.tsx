223c43422658a128b1e3f96f332551349d2bf033
Carlos Mourullo <carlosmourullo@gmail.com>
2023-05-09 13:36:45 +0100

    feat: removed all references to b2b dashboard

--- frontastic/tastics/index.tsx
+++ frontastic/tastics/index.tsx
@@ -40,7 +40,6 @@ import { BusinessUnit } from './business-unit';
 import { BusinessUnitTree } from './business-unit-tree';
 import { StorePickerTastic } from './store-picker';
 import QuoteThankYouTastic from './quotes/thank-you';
-import DashboardTastic from './dashboard';
 
 export const tastics = {
   // Doc Examples
@@ -52,7 +51,6 @@ export const tastics = {
   //'contentful/blog': ContentfulBlog,
 
   'b2b/business-unit': BusinessUnit,
-  'b2b/dashboard': DashboardTastic,
   'b2b/business-unit-tree': BusinessUnitTree,
   'b2b/store-picker': StorePickerTastic,
   'b2b/quote/thank-you': QuoteThankYouTastic,
