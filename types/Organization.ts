f6090ea5a752bd4c8d38e2666df80977ede49067
Carlos Mourullo <carlosmourullo@gmail.com>
2023-08-08 16:04:57 +0200

    chore: marked organization interface as deprecated

--- types/Organization.ts
+++ types/Organization.ts
@@ -2,6 +2,9 @@ import { Store } from '@Types/store/Store';
 import { Channel } from '@Types/store/Channel';
 import { BusinessUnit } from '@Types/business-unit/BusinessUnit';
 
+/**
+ * @deprecated
+ */
 export interface Organization {
   businessUnit: BusinessUnit;
   distributionChannel: Channel;
