beacb5eb4e6b0599cfd7f9e5e0c81eda9b837c60
Carlos Mourullo <carlosmourullo@gmail.com>
2023-08-08 09:58:43 +0200

    refactor: removed method to get attribute groups

--- frontastic/actions/product/index.ts
+++ frontastic/actions/product/index.ts
@@ -8,7 +8,7 @@ export const query = async (search: string): Promise<ProductQueryResponse> => {
   return await fetchApiHub(`/action/product/query?${search}`, { method: 'GET' });
 };
 export const getAttributeGroup = async (key: string): Promise<string[]> => {
-  return await fetchApiHub(`/action/product/getAttributeGroup?key=${key}`, { method: 'GET' });
+  return [];
 };
 
 export const getCategories = (): Category[] => {
